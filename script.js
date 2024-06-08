945bbe88a3a6dc2fff6715d92942d70202264ae6
mellcosta <m3licosta@gmail.com>
2024-06-08 12:39:25 +0530

    Add proper links and target blank

--- script.js
+++ script.js
@@ -2,14 +2,6 @@ function toggleMode(){
     const html = document.documentElement
     html.classList.toggle('dark')
 
-    const img = document.querySelector('#profile img')
-    if(html.classList.contains('dark')){
-        img.setAttribute("src", "./assets/avatar-dark.png")
-    }
-    else{
-        img.setAttribute("src", "./assets/avatar-light.png")
-    }
-
     /*
     Using Conditional for toggle
 
